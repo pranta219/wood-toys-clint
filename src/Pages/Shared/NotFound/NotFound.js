0b4ce30d44c722cffb62104a4278a6afc55d96ed
pranta219 <mdpranta219@gmail.com>
2022-05-21 02:30:08 +0600

    not found page

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import notfound from '../../../img/notfound.jpg'
 
 const NotFound = () => {
     return (
-        
+        <div>
+            <img className='img-fluid' src={notfound} alt="" />
+        </div>
     );
 };
 
